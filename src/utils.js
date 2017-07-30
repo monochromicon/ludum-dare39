71363f10c784450659bea0184719ef120041949e
Graham Barber <grahammbarber@gmail.com>
2017-07-30 01:48:42 -0700

    Added initial dialogue generator

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,26 @@
+import * as characters from './dialog.json'
+
 export const centerGameObjects = objects => {
   objects.forEach(function (object) {
     object.anchor.setTo(0.5)
   })
+}
+
+export const dialogue = function * (name) {
+  const character = characters[name]
+  let currentNode = 0
+
+  let target = yield {
+    line: character[currentNode].line,
+    choices: character[currentNode].responses.map(v => v.quip)
+  }
+
+  while (true) {
+    if (character[currentNode].responses[target].target < 0) return null
+    currentNode = character[currentNode].responses[target].target
+    target = yield {
+      line: character[currentNode].line,
+      choices: character[currentNode].responses.map(v => v.quip)
+    }
+  }
 }
