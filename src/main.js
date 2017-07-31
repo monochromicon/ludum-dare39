9c880bb4ac2ac8d0505d50b712aa57c6f3f77799
Benjamin Arvey <benjamin.arvey@gmail.com>
2017-07-31 16:53:21 -0700

    adaptable controls, death scene, new art

--- src/main.js
+++ src/main.js
@@ -4,29 +4,36 @@ import Phaser from 'phaser'
 
 import BootState from './states/Boot'
 import SplashState from './states/Splash'
+import StartState from './states/Start'
 import GameState from './states/Game'
+import EndState from './states/End'
 
 import config from './config'
 
 class Game extends Phaser.Game {
   constructor () {
     const docElement = document.documentElement
-    const width =
-      docElement.clientWidth > config.gameWidth
-        ? config.gameWidth
-        : docElement.clientWidth
-    const height =
-      docElement.clientHeight > config.gameHeight
-        ? config.gameHeight
-        : docElement.clientHeight
+    const width = docElement.clientWidth > config.gameWidth
+      ? config.gameWidth
+      : docElement.clientWidth
+    const height = docElement.clientHeight > config.gameHeight
+      ? config.gameHeight
+      : docElement.clientHeight
 
     super(width, height, Phaser.CANVAS, 'content', null)
 
     this.state.add('Boot', BootState, false)
     this.state.add('Splash', SplashState, false)
+    this.state.add('Start', StartState, false)
     this.state.add('Game', GameState, false)
+    this.state.add('End', EndState, false)
 
     this.state.start('Boot')
+  }
+
+  die () {
+    console.log('Really Dead')
+    this.state.start('Game')
   }
 }
 
